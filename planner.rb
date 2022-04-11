9b9088672875be8f1050bb8366b1e7ee575bffd6
nik-mak <nick.makrakis@gmail.com>
2022-04-11 10:46:18 +1000

    added readme and command line arguments

--- planner.rb
+++ planner.rb
@@ -2,6 +2,18 @@ require_relative './models/events'
 require_relative './views/planner_view'
 require_relative './controllers/planner_controller'
 
+ARGV.each do |arg|
+  case arg
+  when '-h' || '--help'
+    File.foreach('./files/help.txt') do |each|
+      puts each
+    end
+  when '-v' || '--version'
+    puts 'Hachi version 2.0'
+  end
+  exit
+end
+
 planner_model = Events.new
 planner_view = EventView.new(planner_model)
 planner_controller = PlannerController.new(planner_model, planner_view)
